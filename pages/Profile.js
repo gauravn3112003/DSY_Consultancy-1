01f92ade7c04a59dca48805dcdadf76078c03b17
Gaurav Narnaware <gauravnarnaaware3112003@gmail.com>
2023-01-23 17:33:50 +0530

    create responsive Profile page

--- pages/Profile.js
+++ pages/Profile.js
@@ -23,30 +23,30 @@ const Profile = () => {
         </div>
 
         <div className=" flex flex-wrap mt-3 justify-between items-center">
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">Full Name</div>
             <div className="text-sm">Gaurav Narnaware</div>
           </div>
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">DOB</div>
             <div className="text-sm">03/11/2003</div>
           </div>
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">Social Category</div>
             <div className="text-sm">ST</div>
           </div>
         </div>
 
         <div className=" flex flex-wrap mt-2 justify-between items-center">
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">Gender</div>
             <div className="text-sm">Male</div>
           </div>
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">Marital Status</div>
             <div className="text-sm">Unmaried</div>
           </div>
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">Physically Challenged</div>
             <div className="text-sm">N/A</div>
           </div>
@@ -64,22 +64,22 @@ const Profile = () => {
         </div>
 
         <div className=" flex flex-wrap mt-3 justify-between items-center">
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">Mobile No.</div>
             <div className="text-sm">7796305801e</div>
           </div>
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">E-mail Address</div>
             <div className="text-sm">gauravnarnaware3112003@gmail.com</div>
           </div>
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">City</div>
             <div className="text-sm">Ghatanji</div>
           </div>
         </div>
 
         <div className=" flex flex-wrap mt-2 justify-between items-center">
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">State</div>
             <div className="text-sm">Maharashtra</div>
           </div>
@@ -95,60 +95,60 @@ const Profile = () => {
           <h1 className="text-lg font-bold">Education Details</h1>
           <i className="bi bi-pencil-square font-extrabold text-2xl cursor-pointer "></i>
         </div>
-        <h3 className="text-base text-slate-500 font-semibold">Class X</h3>
+        <h3 className="text-base text-slate-500 sm:font-semibold font-bold ">Class X</h3>
         <div className=" flex flex-wrap mt-3 justify-between items-center">
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">Board</div>
             <div className="text-sm">N/A</div>
           </div>
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">School</div>
             <div className="text-sm">N/A</div>
           </div>
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">Passing Year</div>
             <div className="text-sm">N/A</div>
           </div>
         </div>
 
         <div className=" flex flex-wrap mt-2 justify-between items-center">
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">Mark Type</div>
             <div className="text-sm">N/A</div>
           </div>
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">Percentage/CGPA</div>
             <div className="text-sm">N/A</div>
           </div>
-          <div className="w-2/6" />
+          <div className="w-2/6 detailWrap" />
         </div>
         <div className="border-b-2 mt-5" />
-        <h3 className="text-base text-slate-500 font-semibold mt-5">Diploma</h3>
+        <h3 className="text-base text-slate-500 sm:font-semibold font-bold mt-5">Diploma</h3>
         <div className=" flex flex-wrap mt-3 justify-between items-center">
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">Board</div>
             <div className="text-sm">N/A</div>
           </div>
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">School</div>
             <div className="text-sm">N/A</div>
           </div>
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">Passing Year</div>
             <div className="text-sm">N/A</div>
           </div>
         </div>
 
         <div className=" flex flex-wrap mt-2 justify-between items-center">
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">Mark Type</div>
             <div className="text-sm">N/A</div>
           </div>
-          <div className="w-2/6">
+          <div className="w-2/6 detailWrap">
             <div className="text-slate-400 text-sm">Percentage/CGPA</div>
             <div className="text-sm">N/A</div>
           </div>
-          <div className="w-2/6" />
+          <div className="w-2/6 detailWrap" />
         </div>
       </div>
     );
