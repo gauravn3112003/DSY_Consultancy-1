1ace75f70810cf3a1d6cc476b2fa17a00dc914d8
Gaurav Narnaware <gauravnarnaaware3112003@gmail.com>
2023-02-18 11:38:08 +0530

    add departmetn

--- pages/api/addDepartment.js
+++ pages/api/addDepartment.js
@@ -3,24 +3,30 @@ import Colleges from "../../Modal/Colleges";
 initDB();
 
 export default async (req, res) => {
-  const { courseName, instituteCode, annalFee, choiceCode, cName } = req.body;
-  let dep = {
-    courseName,
-    annalFee,
-    choiceCode,
-    cName,
-  };
+  const { courseName, instituteCode, annalFee, choiceCode } = req.body;
 
   const filter = { instituteCode: instituteCode };
-  const update = { $push: { department: dep } };
 
   try {
     const checkDep = await Colleges.findOne({
       department: { $elemMatch: { choiceCode } },
     });
+
+    const clgName = await Colleges.findOne({
+      instituteCode: instituteCode,
+    });
     if (checkDep) {
       return res.status(422).json({ error: "department already added" });
     }
+
+    let dep = {
+      courseName,
+      annalFee,
+      choiceCode,
+      cName: clgName.name,
+    };
+    const update = { $push: { department: dep } };
+
     const addDep = await Colleges.findOneAndUpdate(filter, update);
     res.status(201).json({ msg: "department Added", addDep });
   } catch (err) {
