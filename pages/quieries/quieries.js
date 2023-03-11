09fe3e5976f1226f4063064df973f7775b566c92
Gaurav Narnaware <gauravnarnaaware3112003@gmail.com>
2023-03-11 10:35:30 +0530

    Add AllCollegeData Page 6

--- pages/quieries/quieries.js
+++ pages/quieries/quieries.js
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useState } from "react";
 export const collegeByUnder = (selectedunder, colleges) => {
   if (!selectedunder.length) {
@@ -5,3 +6,10 @@ export const collegeByUnder = (selectedunder, colleges) => {
   }
   return colleges.filter((under) => selectedunder.includes(under.collegeUnder));
 };
+
+
+const quieries = () => {
+  return <div></div>;
+};
+
+export default quieries;
