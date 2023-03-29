d0a717b88337ed320148d8d161470ad97ebc9527
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2023-03-29 14:54:42 +0530

    Add Serverless property

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  serverless: {
+    // Increase the timeout limit to 30 seconds
+    timeout: 30,
+  },
   images: {
     remotePatterns: [
       {
