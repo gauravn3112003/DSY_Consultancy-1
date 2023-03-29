27586f9e15382335726db27925b07d6eafc1eb9b
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2023-03-29 14:59:44 +0530

    Add Serverless property 2

--- pages/api/User/[id].js
+++ pages/api/User/[id].js
@@ -2,6 +2,7 @@ import initDB from "../../../Helpers/initDB";
 import User from "directsecondyearadmission/Modal/User";
 initDB();
 export default async function handler(req, res) {
+  res.setHeader("Cache-Control", "s-maxage=3600, stale-while-revalidate");
   try {
     const { id } = req.query;
     const user = await User.findById(id);
